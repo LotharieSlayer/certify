d3b3feb3995b5a6c4437ef48d4619958a354231d
LotharieSlayer <lothaireguee.pro@outlook.fr>
2023-04-01 15:06:29 +0000

    Mise à jour du message de certification

--- files/welcomeMessages.js
+++ files/welcomeMessages.js
@@ -6,8 +6,8 @@ exports.VERIFIED =
         icon_url: 'https://cdn.discordapp.com/avatars/784797683160317963/81d72ff0ef0b00779844d43ece16d096.webp'
         },
         title: `**Bravo ! Tu viens d'être certifié !**`,
-        description: `Tu peux maintenant publier des memes et aller découvrir gratuitement les channels du niveau 5 en suivant : <#804013770728407110> !
-        **N'oublies pas de liker les memes avec <:fmLove:781475597129220096> !**`,
+        description: "Tu peux maintenant publier des memes et aller débloquer les channels NSFW s'ils t'intéressent dans `Salons et rôles` !\n" +
+        "**N'oublies pas de liker les memes avec <:fmLove:781475597129220096> !**",
         timestamp: new Date(),
         footer: {
             icon_url: 'https://cdn.discordapp.com/attachments/760963894469722142/904877119463624764/chieng-512.gif',
